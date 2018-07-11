30347cf7b7722e6ef540c5d47cd91ddad89b5c7b
Ian Lo <ianlo196@gmail.com>
2018-07-11 19:14:08 +0000

    Added set -o nounset, changed to using the new conda sh script

--- scripts/install_latest_stable_freenome_build_local.sh
+++ scripts/install_latest_stable_freenome_build_local.sh
@@ -9,6 +9,7 @@ isZsh() {
  }
 
 set -e
+set -o nounset
 
 ANACONDA_TOKEN=$1
 if [ $(uname) = 'Linux' ]; then
@@ -33,7 +34,7 @@ if [[ $CONDA_INSTALLED -eq 0 ]]; then
     curl -sO $MINICONDA_URL;
     bash $(basename $MINICONDA_URL) -b -u -p $MINICONDA_INSTALL_PATH;
     # activate the base conda environment
-    export PATH="$MINICONDA_INSTALL_PATH/bin:$PATH";
+    source "$MINICONDA_INSTALL_PATH/etc/profile.d/conda.sh";
     conda install anaconda-client --yes
 fi
 
