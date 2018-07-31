75b3feb7da696e2788bf34df6b6a4e6326845c3b
Ian Lo <ianlo196@gmail.com>
2018-07-31 21:45:56 +0000

    Added start, run_migrations, insert_test_data, reset_test_data, stop

--- freenome_build/db.py
+++ freenome_build/db.py
@@ -1,15 +1,14 @@
-import os
-import time
-import subprocess
 import logging
+import os
 import psycopg2
+import socket
+import subprocess
+import time
+from contextlib import closing
 
 from freenome_build.util import norm_abs_join_path, change_directory, get_git_repo_name, run_and_log
 
 logger = logging.getLogger(__file__)  # noqa: invalid-name
-
-DEFAULT_TEST_DB_PORT = 3333
-DEFAULT_TEST_DB_HOST = 'localhost'
 
 # the maximum amount of time in seconds to wait for the DB to come up before raising an error
 MAX_DB_WAIT_TIME = 10
@@ -23,7 +22,13 @@ def _execute_sql_script(sql_script_path, dbuser, dbname, host, port):
     pass
 
 
-def _setup_db(image_name, repo_path, host, port):
+def _find_free_port():
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+        s.bind(('', 0))
+        return s.getsockname()[1]
+
+
+def _setup_db(repo_path: str, host: str, port: int, project_name: str = None) -> None:
     # check if 'setup' exists in repo_path/database/
     repo_setup_sql_path = norm_abs_join_path(repo_path, "./database/setup.sql")
     if os.path.exists(repo_setup_sql_path):
@@ -35,13 +40,14 @@ def _setup_db(image_name, repo_path, host, port):
             os.path.dirname(__file__), "./database_template/scripts/setup.sql")
         logger.debug(f"The repo at '{repo_path}' does not contain './database/setup.sql'."
                      f"\nDefaulting to {setup_sql_template_path} with "
-                     f"USER='{image_name}', DATABASE='{image_name}'")
+                     f"USER='{project_name}', DATABASE='{project_name}'")
         # get the new database password from the environment
-        if 'PGPASSWORD' not in os.environ:
-            raise ValueError("PGPASSWORD must be in the environment to setup a new database.")
+        password_env_var = f"{project_name}_DB_PASSWORD"
+        if password_env_var not in os.environ:
+            raise ValueError(f"{password_env_var} must be in the environment to setup a new database.")
         with open(setup_sql_template_path) as ifp:
             setup_sql = ifp.read().format(
-                PGUSER=image_name, PGDATABASE=image_name, PGPASSWORD=os.environ['PGPASSWORD']
+                PGUSER=project_name, PGDATABASE=project_name, PGPASSWORD=os.environ[password_env_var]
             )
 
     # execute the setup.sql script
@@ -50,7 +56,67 @@ def _setup_db(image_name, repo_path, host, port):
     return
 
 
-def _run_migrations(repo_path, host, port, dbname, dbuser):
+def _wait_for_db_cluster_to_start(host: str, port: int, max_wait_time=MAX_DB_WAIT_TIME, recheck_interval=0.2) -> None:
+    conn_str = f"dbname=postgres user=postgres host={host} port={port}"
+    for _ in range(int(max_wait_time/recheck_interval)+1):
+        try:
+            with psycopg2.connect(conn_str) as _: # noqa
+                # we just want to see if we can connect, so if we do then connect
+                logger.debug(f"Database at '{conn_str}' is up!")
+                return
+        except psycopg2.OperationalError:
+            logger.debug(f"DB cluster at '{conn_str}' is not yet up.")
+            time.sleep(recheck_interval)
+            continue
+
+    raise RuntimeError(f"Aborting because the DB did not start within {MAX_DB_WAIT_TIME} seconds.")
+
+
+def start_local_database(repo_path: str, project_name: str, port: int = None) -> None:
+    """Start a test database in a docker container.
+
+    This starts a new test database in a docker container. This function:
+    1) builds the postgres server docker image
+    2) starts the docker container on port 'port'
+    """
+    # set the path to the Postgres Dockerfile
+    docker_file_path = norm_abs_join_path(repo_path, "./database/Dockerfile")
+    # if the repo doesn't have a Dockerfile in the database sub-directory, then
+    # default to the template Dockerfile
+    if not os.path.exists(docker_file_path):
+        docker_file_path = norm_abs_join_path(
+            os.path.dirname(__file__), "./database_template/Dockerfile")
+        logger.info(f"Setting DB docker file path to '{docker_file_path}'")
+
+    docker_file_dir = os.path.dirname(docker_file_path)
+
+    # build
+    build_cmd = f"docker build --rm -t {project_name}:latest {docker_file_dir}"
+    run_and_log(build_cmd)
+
+    # Find a free port if one wasn't specified
+    if port is None:
+        port = _find_free_port()
+    # starting db
+    run_cmd = f"docker run -d -p {port}:5432 --name {project_name}_{port} {project_name}:latest"
+    run_and_log(run_cmd)
+
+    # Wait for the db to start up before configuring it
+    _wait_for_db_cluster_to_start('localhost', port)
+
+    try:
+        # Set up the database
+        _setup_db(repo_path, 'localhost', port, project_name)
+
+        # Try connecting to the new database
+        connection_cmd = f"psql -h localhost -p {port} -U {project_name} -d {project_name}"
+        logger.info(f"Database is up! You can connect by running:\n{connection_cmd}")
+    except Exception:
+        stop_local_database(project_name, port)
+        raise
+
+
+def run_migrations(repo_path: str, host: str, port: int, dbname: str, dbuser: str) -> None:
     # check if 'migrate' exists in repo_path/database/
     repo_migrate_path = norm_abs_join_path(repo_path, "./database/migrate")
     if os.path.exists(repo_migrate_path):
@@ -78,8 +144,8 @@ def _run_migrations(repo_path, host, port, dbname, dbuser):
     return
 
 
-def _insert_test_data(repo_path, host, port, dbuser, dbname):
-    # check if 'migrate' exists in repo_path/database/
+def insert_test_data(repo_path: str, host: str, port: str, dbuser: str, dbname: str) -> None:
+    # check if 'insert_test_data' exists in repo_path/database/
     repo_insert_test_data_path = norm_abs_join_path(
         repo_path, "./database/insert_test_data")
     if os.path.exists(repo_insert_test_data_path):
@@ -106,25 +172,21 @@ def _insert_test_data(repo_path, host, port, dbuser, dbname):
     raise ValueError(f"'{repo_path}' does not contain an insert test data script or sql file.")
 
 
-def _wait_for_db_cluster_to_start(host, port, max_wait_time=MAX_DB_WAIT_TIME, recheck_interval=0.2):
-    conn_str = f"dbname=postgres user=postgres host={host} port={port}"
-    for _ in range(int(max_wait_time/recheck_interval)+1):
-        try:
-            with psycopg2.connect(conn_str) as _: # noqa
-                # we just want to see if we can connect, so if we do then connect
-                logger.debug(f"Database at '{conn_str}' is up!")
-                return
-        except psycopg2.OperationalError:
-            logger.debug(f"DB cluster at '{conn_str}' is not yet up.")
-            time.sleep(recheck_interval)
-            continue
-
-    raise RuntimeError(f"Aborting because the DB did not start within {MAX_DB_WAIT_TIME} seconds.")
+def reset_test_data(host: str, port: str, user: str, dbname: str) -> None:
+    conn = psycopg2.connect(host=host, port=port, user=user, dbname=dbname)
+    cursor = conn.cursor()
+    # Select the namespace and the tablename ignoring any tables that start
+    # with pg_ and sql_ and any sqitch namespaces
+    get_all_tables_sql = "select nspname||'.'||relname from pg_class join pg_namespace on " \
+                         "relnamespace = pg_namespace.oid where relkind='r' and " \
+                         "relname !~ '^(pg_|sql_)' and nspname != 'sqitch';"
+    cursor.execute(get_all_tables_sql)
+    tables = cursor.fetchall()
+    for table in tables:
+        cursor.execute(f'delete from {table[0]}')
 
 
-def stop_test_database(project_name, host=DEFAULT_TEST_DB_HOST, port=DEFAULT_TEST_DB_PORT):
-    if host != 'localhost':
-        raise NotImplementedError('Non localhost test databases are not supported.')
+def stop_local_database(project_name: str, port: int) -> None:
     image_name = f"{project_name}_{port}"
     cmd = f"docker kill {image_name}"
     try:
@@ -144,109 +206,40 @@ def stop_test_database(project_name, host=DEFAULT_TEST_DB_HOST, port=DEFAULT_TES
     run_and_log(cmd)
 
 
-def configure_test_database(host, port, project_name, repo_path):
-    # the database cluster needs some time to start, so try to connect periodically until we can
-    _wait_for_db_cluster_to_start(host, port)
-
-    # setup-db
-    # we need to connect to the 'postgres' database to create a new database
-    _setup_db(project_name, repo_path, host, port)
-
-    # run-migrations
-    _run_migrations(
-        repo_path=repo_path, host=host, port=port, dbname=project_name, dbuser=project_name)
-
-    # insert test data
-    _insert_test_data(
-        repo_path=repo_path, host=host, port=port, dbname=project_name, dbuser=project_name)
+def start_local_database_main(args):
+    start_local_database(args.path, args.project_name, args.port)
 
 
-def start_test_database(
-        repo_path, project_name, host=DEFAULT_TEST_DB_HOST, port=DEFAULT_TEST_DB_PORT):
-    """Start a test database in a docker container.
-
-    This starts a new test database in a docker container. This function:
-    1) builds the postgres server docker image
-    2) starts the docker container on port 'port'
-    3) waits for the database cluster to start
-    4) runs the DB setup script
-       - create a new db user $dbname
-       - create a new db $dbname owned by $dbname
-    5) runs the database migrations
-    """
-    # TODO (nb): add a check to ensure that 'port' is free
-    # host is always localhost because we are running it in a local Docker container
-    if host != 'localhost':
-        raise NotImplementedError('Non localhost test databases are not supported.')
-
-    # set the path to the Postgres Dockerfile
-    docker_file_path = norm_abs_join_path(repo_path, "./database/Dockerfile")
-    # if the repo doesn't have a Dockerfile in the database sub-directory, then
-    # default to the template Dockerfile
-    if not os.path.exists(docker_file_path):
-        docker_file_path = norm_abs_join_path(
-            os.path.dirname(__file__), "./database_template/Dockerfile")
-        logger.info(f"Setting DB docker file path to '{docker_file_path}'")
-
-    docker_file_dir = os.path.dirname(docker_file_path)
-
-    # build
-    cmd = f"docker build --rm -t {project_name}:latest {docker_file_dir}"
-    run_and_log(cmd)
-
-    # starting-db
-    cmd = f"docker run -d -p {port}:5432 --name {project_name}_{port} {project_name}:latest"
-    run_and_log(cmd)
-
-    # Set up the database, run migrations, insert test data
-    configure_test_database(host, port, project_name, repo_path)
-
-    # Inject test-db credentials into the env for easy integration
-    os.environ['PGHOST'] = host
-    os.environ['PGPORT'] = str(port)
-    os.environ['PGUSER'] = project_name
-    os.environ['PGDATABASE'] = project_name
-
-    # log the connection command
-    connection_cmd = f"psql -h {host} -p {port} -U {project_name} {project_name}"
-    logger.info(f"Database is up! You can connect by running:\n{connection_cmd}")
+def run_migrations_main(args):
+    run_migrations(args.path, args.host, args.port, args.project_name, args.project_name)
 
 
-def start_test_database_main(args):
-    if args.force:
-        # if the container does not exist then we don't need to stop anything
-        try:
-            stop_test_database(args.project_name, args.host, args.port)
-        except ContainerDoesNotExistError:
-            pass
-    start_test_database(args.path, args.project_name, args.host, args.port)
+def insert_test_data_main(args):
+    insert_test_data(args.path, args.host, args.port, args.project_name, args.project_name)
 
 
-def stop_test_database_main(args):
-    stop_test_database(args.project_name, args.host, args.port)
+def reset_test_data_main(args):
+    reset_test_data(args.host, args.port, args.project_name, args.project_name)
 
 
-def connect_to_test_db_main(project_name, host, port):
-    connection_cmd = f"psql -h {host} -p {port} -U {project_name} {project_name}"
-    logger.debug(f"Running '{connection_cmd}'")
-    proc = subprocess.Popen(connection_cmd, shell=True)
-    proc.wait()
+def stop_local_database_main(args):
+    stop_local_database(args.project_name, args.port)
 
 
 def add_db_subparser(subparsers):
-    database_parser = subparsers.add_parser('test-db', help='manage the test database')
+    database_parser = subparsers.add_parser('db', help='manage the test database')
     database_parser.required = True
     database_parser.add_argument('--path', default='.')
     database_parser.add_argument(
-        '--port', default=DEFAULT_TEST_DB_PORT,
-        help='Port on which to start the test db. Default: %(default)s'
+        '--port',
+        help='Port on which to start the test db. Default is a random free port'
     )
     database_parser.add_argument(
-        '--host', default=DEFAULT_TEST_DB_HOST,
+        '--host', default='localhost',
         help='Teste DB host. Default: %(default)s'
     )
     database_parser.add_argument(
-        '--project-name',
+        '--project_name',
         default=None,
         help='The name of the project.\n'
              'This is assumed to be the name of the database and the database owner.\n'
@@ -258,18 +251,21 @@ def add_db_subparser(subparsers):
     database_subparsers.required = True
 
     # Start a test DB
-    start_test_db_parser = database_subparsers.add_parser('start', help='start a test database')
-    start_test_db_parser.add_argument(
-        '--force', '-f', action='store_true', default=False,
-        help='Start the test database even if it means stopping an already existing '
-             'docker container of the same name.'
-    )
+    database_subparsers.add_parser('start', help='start a test database')
+
+    # Run the sqitch migrations on the database
+    database_subparsers.add_parser('run_migrations', help='Run migrations on database schemas')
+
+    # Insert test data into the database
+    database_subparsers.add_parser('insert_test_data', help='Insert test data into the database')
+
+    # Reset the database to its starting point.
+    database_subparsers.add_parser('reset_test_data',
+                                   help='Reset the data in the test database.'
+                                   'Requires inserting the test data afterwards.')
 
     # Stop the test db
     database_subparsers.add_parser('stop', help='stop the test database')
-
-    # Connect to a test db
-    database_subparsers.add_parser('connect', help='Connect to the test database.')
 
 
 def db_main(args):
@@ -281,11 +277,15 @@ def db_main(args):
         args.project_name = get_git_repo_name(args.path).replace('-', '_')
         logger.info(f"Setting project name to '{args.project_name}'")
 
-    if args.test_db_command == 'connect':
-        connect_to_test_db_main(args.project_name, args.host, args.port)
     elif args.test_db_command == 'start':
-        start_test_database_main(args)
+        start_local_database_main(args)
+    elif args.test_db_command == 'run_migrations':
+        run_migrations_main(args)
+    elif args.test_db_command == 'insert_test_data':
+        insert_test_data_main(args)
+    elif args.test_db_command == 'reset_test_data':
+        reset_test_data_main(args)
     elif args.test_db_command == 'stop':
-        stop_test_database_main(args)
+        stop_local_database_main(args)
     else:
         raise ValueError(f"Unrecognized DB subcommand '{args.test_db_command}'")
