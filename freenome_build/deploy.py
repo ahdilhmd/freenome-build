4b3c251235c6cb056928ef348ae92f0a432ec816
drakeeee <adam.drake137@gmail.com>
2018-08-30 16:10:03 -0700

    add options to specify package name and build for all platforms to freenome-build deploy

--- freenome_build/deploy.py
+++ freenome_build/deploy.py
@@ -8,25 +8,35 @@ from freenome_build import version_utils
 LOCAL_CONDA_BUILD_SCRIPT = os.path.abspath('scripts/conda_build.sh')
 
 
-def build_and_upload_package_from_repo(path='./', upload=True, skip_existing=False, repo_name=None):
+def build_and_upload_package_from_repo(path='./', upload=True, skip_existing=False, package_name=None, all=False):
     """
 
     Args:
         path (str): library path to install (default './')
         upload (bool): upload to freenome conda channel (default True)
         skip_existing (bool): do not build if existing build in local conda install (default False)
-        repo_name (str): repo name to build (optional)
+        package_name (str): name to build package into upon upload to freenome conda (optional)
+        all (bool): build conda package for all platforms
 
     Returns:
         None
 
     """
     version = version_utils.version(path)
+
     output_file_path = build_package(path, version, skip_existing=skip_existing)
 
     if upload:
         upload_cmd = ['anaconda', '-t', os.environ['ANACONDA_TOKEN'],
-                      'upload', '--force', '-u', 'freenome', output_file_path]
+                      'upload', '--force', '-u']
+
+        if package_name is not None:
+            upload_cmd += ['--package', package_name]
+
+        if all:
+            upload_cmd += ['--all']
+
+        upload_cmd +=['freenome', output_file_path]
 
         subprocess.check_call(upload_cmd, stdout=sys.stdout, stderr=sys.stderr)
 
@@ -35,7 +45,9 @@ def deploy_main(args):
     return build_and_upload_package_from_repo(
         path=args.path,
         upload=args.upload,
-        skip_existing=args.skip_existing
+        skip_existing=args.skip_existing,
+        package_name=args.package_name,
+        all=args.all
     )
 
 
@@ -48,3 +60,7 @@ def add_deploy_subparser(subparsers):
         '-p', '--path', action='store', default='./', dest='path')
     deploy_subparser.add_argument(
         '--skip', action='store_true', default=False, dest='skip_existing')
+    deploy_subparser.add_argument(
+        '-r', '--package_name', action='store')
+    deploy_subparser.add_argument(
+        '-a', '--all', action='store_true')
